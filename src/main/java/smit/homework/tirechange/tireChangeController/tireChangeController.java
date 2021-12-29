dcdf0d9c722385d5f0092daf22d5b3553e139f83
Sander Kondratjev <sanderko@hotmail.com>
2021-12-29 15:08:55 +0200

    Improve booking data from london server

--- src/main/java/smit/homework/tirechange/tireChangeController/tireChangeController.java
+++ src/main/java/smit/homework/tirechange/tireChangeController/tireChangeController.java
@@ -3,7 +3,6 @@ package smit.homework.tirechange.tireChangeController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import smit.homework.tirechange.tireChangeService.tireChangeService;
@@ -22,12 +21,9 @@ public class tireChangeController {
     }
 
     @GetMapping("tire-change/london-booking-times")
-    public String booking() {
-
-        final String uri = "http://localhost:9003/api/v1/tire-change-times/available?from=2006-01-02&until=2030-01-02";
-
+    public String getFromUrl() {
         RestTemplate restTemplate = new RestTemplate();
-
-        return restTemplate.getForObject(uri, String.class);
+        return restTemplate.getForObject("http://localhost:9003/api/v1/tire-change-times/available?from=2006-01-02&until=2030-01-02",
+                String.class);
     }
 }
