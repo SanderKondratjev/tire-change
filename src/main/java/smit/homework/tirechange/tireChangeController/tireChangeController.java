7c59c8ca6fea18986287e225243dd01d1a6448fd
Sander Kondratjev <sanderko@hotmail.com>
2021-12-29 13:38:57 +0200

    Test booking data from london server

--- src/main/java/smit/homework/tirechange/tireChangeController/tireChangeController.java
+++ src/main/java/smit/homework/tirechange/tireChangeController/tireChangeController.java
@@ -2,7 +2,10 @@ package smit.homework.tirechange.tireChangeController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import smit.homework.tirechange.tireChangeService.tireChangeService;
 
 @CrossOrigin
@@ -12,5 +15,19 @@ public class tireChangeController {
     @Autowired
     private tireChangeService tireChangeService;
 
+    @GetMapping("tire-change")
+    public String info() {
 
+        return "Tire-change booking system";
+    }
+
+    @GetMapping("tire-change/london-booking-times")
+    public String booking() {
+
+        final String uri = "http://localhost:9003/api/v1/tire-change-times/available?from=2006-01-02&until=2030-01-02";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.getForObject(uri, String.class);
+    }
 }
